49bae34294bf1374dd7bc4b1bb89f847ab65e1fe
jusko1505 <justinkousa@gmaillcom>
2023-02-22 12:20:23 -0800

    hello world

--- mavendemo/src/main/java/com/example/App.java
+++ mavendemo/src/main/java/com/example/App.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import com.google.common.base.Joiner;
 /**
  * Hello world!
  *
@@ -9,5 +10,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Joiner joiner = Joiner.on("").skipNulls();
+
+        String str = joiner.join("H", null, 'e', 'l', 'l', 'o', "world");
+        
+        System.out.println(str);
+    
     }
 }
